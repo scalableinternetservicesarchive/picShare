297fea99440188edb849d92c47972322563bc3cb
Erik J <erikgunnar.jansen@hotmail.com>
2014-10-29 17:43:56 +0100

    Added validation for fields of posts in its model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :postVotes
 	has_many :users, through: :postVotes
+
+	validates :title, :description, :image_url, :upvotecount, :downvotecount, :postdate, presence: true
+	validates :upvotecount, :downvotecount, numericality: {greater_than_or_equal_to: 0}
 end
 
 class PostVotes < ActiveRecord::Base
