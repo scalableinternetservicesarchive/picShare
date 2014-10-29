97030cf4a1624890ead4bced6cfb4e4f82e37ed7
Erik J <erikgunnar.jansen@hotmail.com>
2014-10-29 23:27:30 +0100

    added validation for image urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ActiveRecord::Base
 
 	validates :title, :description, :image_url, :upvotecount, :downvotecount, :postdate, presence: true
 	validates :upvotecount, :downvotecount, numericality: {greater_than_or_equal_to: 0}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'Must be a URL for GIF, JPG, or PNG image'
+	}
 end
 
 class PostVotes < ActiveRecord::Base
