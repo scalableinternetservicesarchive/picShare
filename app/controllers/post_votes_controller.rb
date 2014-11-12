d77334145c78e3bc1879437b9af3088c77bb5e6c
Erik J <erikgunnar.jansen@hotmail.com>
2014-11-12 01:57:56 +0100

    removed redudant code

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -71,13 +71,12 @@ class PostVotesController < ApplicationController
       @num_of_not_received = User.count - (@alreadyReceived.count)
 
       # If there are enough who receiveres --> send nrOfReceivers users
+      potentialReceivers = User.where.not(id: @alreadyReceived)
       if @num_of_not_received >= nrOfReceivers
-        potentialReceivers = User.where.not(id: @alreadyReceived)
         @receivers = potentialReceivers.sample(nrOfReceivers)
       end
       # If there are receivers, but not as many as nrOfReceivers --> send to rest of users
       else if @num_of_not_received < nrOfReceivers and @num_of_not_received > 0
-        potentialReceivers = User.where.not(id: @alreadyReceived)
         @receivers = potentialReceivers.sample(@num_of_not_received)
       end
 
