5bc20357f0f36939d935b570fb11a181f7320088
Erik J <erikgunnar.jansen@hotmail.com>
2014-11-12 01:07:24 +0100

    pickReceiver method is now identical in post_controller and post_votes_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,10 +24,12 @@ class PostsController < ApplicationController
     #@post.user = current_user
     @post = current_user.posts.build(post_params)
     @post.save
-    @users = pickReceivers(@post.user_id, @post.id, User.count - 1)
-    @users.each do |user|  
-      @post_vote = PostVote.create(user_id: user.id, post_id: @post.id, vote: 0)
-      @post_vote.save
+    @receivers = pickReceivers(@owner, @post_vote.post_id, 2)
+    if @receivers != nil
+      @receivers.each do |receiver|
+        @post_vote = PostVote.create(user_id: receiver.id, post_id: @post.id, vote: 0)
+        @post_vote.save
+      end
     end
     respond_with(@post)
   end
@@ -57,8 +59,18 @@ class PostsController < ApplicationController
       @alreadyReceived = @postVotes.map {|postVote| postVote.user_id}
       @alreadyReceived.push(post_owner)
 
-      potentialReceivers = User.where.not(id: @alreadyReceived)
-      @receivers = potentialReceivers.sample(nrOfReceivers)
+      @num_of_not_received = User.count - (@alreadyReceived.count)
+
+      # If there are enough who receiveres --> send nrOfReceivers users
+      if @num_of_not_received >= nrOfReceivers
+        potentialReceivers = User.where.not(id: @alreadyReceived)
+        @receivers = potentialReceivers.sample(nrOfReceivers)
+      end
+      # If there are receivers, but not as many as nrOfReceivers --> send to rest of users
+      else if @num_of_not_received < nrOfReceivers and @num_of_not_received > 0
+        potentialReceivers = User.where.not(id: @alreadyReceived)
+        @receivers = potentialReceivers.sample(@num_of_not_received)
+      end
 
       return @receivers
     end
