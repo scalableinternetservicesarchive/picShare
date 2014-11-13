214c8b2b085faf0e182324d5daea1826810f13d0
sivapalan <varuns@stud.ntnu.no>
2014-11-12 19:49:42 -0800

    Set size for attached image

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	has_many :post_votes
 	has_many :users, through: :post_votes
 	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :original => "640x640>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	
 	validates :title, :image, :description, :upvotecount, :downvotecount, :postdate, presence: true
