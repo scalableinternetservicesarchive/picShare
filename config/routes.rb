b089389d48221247ebeb8545a479bb9b1e09876e
sivapalan <varuns@stud.ntnu.no>
2014-11-20 11:23:47 -0800

    Set 'Top Posts' page as root and added user email in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'home#topposts'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
