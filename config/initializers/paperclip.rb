22438bf879de9ee153c9509e284799ccdfb9bfee
Vikas Gupta <gvikas91@gmail.com>
2014-12-05 21:28:35 -0800

    ignore paperclip

--- config/initializers/paperclip.rb
+++
@@ -1,6 +0,0 @@
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-#Paperclip::Attachment.default_options[:storage] = :fog
-#Paperclip::Attachment.default_options[:fog_credentials] = {:use_iam_profile => true, :provider => 'AWS', :region => 'us-west-2'}
-#Paperclip::Attachment.default_options[:fog_directory] = 'cs290'
-#Paperclip::Attachment.default_options[:path] = "picShare/:class/:attachment/:id/:style/:filename"
- 
