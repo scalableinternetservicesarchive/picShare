c468639f7fcd758ae3d36fded2ff554879c6abe4
Vikas Gupta <gvikas91@gmail.com>
2014-12-06 19:31:44 -0800

    Edited seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,28 +11,9 @@ users = User.create([
 	{ email: 'testC@mail.com', password: '34567890', password_confirmation: '34567890'},
 	{ email: 'testD@mail.com', password: '12345678', password_confirmation: '12345678'},
 	{ email: 'testE@mail.com', password: '12345678', password_confirmation: '12345678'}])
-=begin
-posts = Post.create([
-  {user_id: 1, title: "Tittel_A1", description: "Desc_A1", image_url: "a1.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 1, title: "Tittel_A2", description: "Desc_A2", image_url: "a2.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 1, title: "Tittel_A3", description: "Desc_A3", image_url: "a3.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
 
-  {user_id: 2, title: "Tittel_B1", description: "Desc_B1", image_url: "b1.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 2, title: "Tittel_B2", description: "Desc_B2", image_url: "b2.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 2, title: "Tittel_B3", description: "Desc_B3", image_url: "b3.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
 
-  {user_id: 3, title: "Tittel_C1", description: "Desc_C1", image_url: "c1.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 3, title: "Tittel_C2", description: "Desc_C2", image_url: "c2.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now},
-  {user_id: 3, title: "Tittel_C3", description: "Desc_C3", image_url: "c3.jpg", upvotecount: 0, downvotecount: 0, postdate: Time.now}])
-
-post_votes = PostVote.create([
-	{user_id: 1, post_id: 4, vote: 0},
-	{user_id: 1, post_id: 9, vote: 0},
-	{user_id: 1, post_id: 7, vote: 0},
-	{user_id: 2, post_id: 1, vote: 0},
-	{user_id: 2, post_id: 3, vote: 0},
-	{user_id: 2, post_id: 8, vote: 0},
-	{user_id: 3, post_id: 2, vote: 0},
-	{user_id: 3, post_id: 5, vote: 0},
-	{user_id: 3, post_id: 6, vote: 0}])
-=end
+(0..5).each do |i|
+	p = Post.create([user_id: i, title:"Title!", description: "Desc_C3", image: File.open('http://placehold.it/640x640&text=Test%20Image'), postdate: Time.now)
+	p.save!
+end 
