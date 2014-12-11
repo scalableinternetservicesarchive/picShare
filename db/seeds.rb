f714f30c830c21277f759c8380f2c71ab1b19bee
Vikas Gupta <gvikas91@gmail.com>
2014-12-11 12:01:25 -0800

    some more fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-userCount = 10
-postPrUserCount = 5
+userCount = 50000
+postPrUserCount = 1
 postVotesPrPostCount = 3
 
 pic =File.open('/home/ec2-user/app/kitty.jpg')
+p_original = Post.create(user_id: 1, title:"Title!", description: "Desc_C3", image: pic, postdate: Time.now)
 
 (1..userCount).each do |i| 
 	user = User.create(email: i.to_s+'@mail.com', password: '12345678', password_confirmation: '12345678')
 	(1..postPrUserCount).each do |j|
-		p = Post.create(user_id: i, title:"Title!", description: "Desc_C3", image: pic, postdate: Time.now)
+		p = Post.create(user_id: i, title:"Title!", description: "Desc_C3", image: p_original.image, postdate: Time.now)
 			(1..postVotesPrPostCount).each do |k|
 				postVote = PostVote.create(user_id: (i+k)%userCount, post_id: p.id)
 		end
