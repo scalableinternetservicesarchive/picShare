c9502172305819cdf44c2bdbbd547dc72d01f42a
janzh <erikgunnar.jansen@hotmail.com>
2014-12-12 04:25:30 +0100

    Added eager loadin in home_controller to reduce #of queries in inbox, myposts and topposts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def inbox
-  	@post_votes = PostVote.where(user: current_user).where(vote: 0)
+  	@post_votes = PostVote.includes(:post).where(user: current_user).where(vote: 0)
   end
 
   def new
@@ -8,11 +8,11 @@ class HomeController < ApplicationController
   end
 
   def myposts
-  	@posts = Post.where(user_id: current_user)
+  	@posts = Post.includes(:image, :title, :postdate, :upvotecount, :downvotecount).where(user_id: current_user)
   end
 
   def topposts
-    @posts = Post.order('upvoteCount - downvoteCount DESC').limit(10)
+    @posts = Post.includes(:image, :title, :postdate, :upvotecount, :downvotecount).order('upvoteCount - downvoteCount DESC').limit(10)
   end
 
   def index
